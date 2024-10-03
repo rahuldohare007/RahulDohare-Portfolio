51cf19f78264e10a186bd16e6f3b8133dc4b2054
rahuldohare007 <rahuldohare401@gmail.com>
2024-10-03 12:08:46 +0530

    Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,30 +1,4 @@
-// import { FaLinkedin, FaTwitter, FaInstagram } from "react-icons/fa";
-
-// export default function Footer() {
-//   return (
-//     <div className="px-6 md:px-0 mt-12 text-white/70 py-8 max-w-[1000px] mx-auto border-t border-gray-700 pt-4 flex justify-between items-center"  id="contact">
-//       <h1 className="text-2xl font-bold">Rahul Dohare</h1>
-//       <h2 className="text-xl text-gray-500 mt-2">© 2019 - 2024 Cyborgcoding. All Right Reserved.</h2>
-//       <div className="flex space-x-6 mt-2">
-//         <a href="https://www.linkedin.com/in/rahul-dohare-0a5a1619a/" className="hover:text-blue-700">
-//           <FaLinkedin size={24} />
-//         </a>
-//         <a href="#" className="hover:text-red-700">
-//           <FaInstagram size={24} />
-//         </a>
-//         <a href="https://www.x.com/rahuldoharepoet" className="hover:text-blue-600">
-//           <FaTwitter size={24} />
-//         </a>
-//       </div>
-//     </div>
-//   );
-// }
-
-
-
-
-
-import { FaLinkedin, FaTwitter, FaInstagram } from "react-icons/fa";
+import { FaLinkedin, FaTwitter,FaGithub, FaInstagram } from "react-icons/fa";
 
 export default function Footer() {
   return (
@@ -47,6 +21,9 @@ export default function Footer() {
         </a>
         <a href="https://www.instagram.com/cyborgcoding" className="hover:text-red-700">
           <FaInstagram size={24} />
+        </a>
+        <a href="https://github.com/rahuldohare007" className="hover:text-red-700">
+          <FaGithub size={24} />
         </a>
         <a href="https://www.x.com/rahuldoharepoet" className="hover:text-blue-600">
           <FaTwitter size={24} />
